9e596cbb94bb010d51389a8e2aeb942f7d076af4
Tirtha2512 <tirthagohel159@gmail.com>
2026-01-13 08:30:39 +0530

    Add files via upload

---
+++ operator.py
@@ -0,0 +1,43 @@
+#arithmetic operator
+a=10
+b=65
+print(a+b)
+print(a-b)
+print(a/b)
+print(a*b)
+#assignment operator
+x=10
+print(x)
+#unary minus
+a=5
+print(-a)
+#relational operator
+p=16
+q=45
+print(p>q)
+print(p<q)
+print(p==q)
+#logical operators
+x=True
+y=False
+print(x and y)
+print(x or y)
+print(not x)
+print(not y)
+#bitwise operators
+a=5
+b=6
+print(a&b)
+print(a/b)
+#membership operators
+numbers=[1,2,3,4,5]
+print(2 in numbers)
+print(5 not in numbers)
+#identity opertors
+x=5
+y=12
+print(x is y)
+print(x is not y)
+#boolean 
+is_pass
+print(is_pass)
