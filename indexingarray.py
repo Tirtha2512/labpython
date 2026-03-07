2a27a8ace160fadb65d5127217322affb719dc02
Tirtha2512 <tirthagohel159@gmail.com>
2026-03-07 09:20:39 +0530

    Add files via upload

---
+++ indexingarray.py
@@ -0,0 +1,18 @@
+#positive Indexing
+from array import array
+arr=array("i",[10,20,30,40,50])
+print(arr[0])
+print(arr[2])
+print(arr[4])
+#Negative Indexing
+from array import array
+arr=array("i",[10,20,30,40,50])
+print(arr[-1])
+print(arr[-2])
+print(arr[-5])
+#modifying Element using Index
+arr=array("i",[10,20,30,40,50])
+arr[2]=35
+print(arr)
+arr=array("i",[10,20,30])
+print(arr[5])
